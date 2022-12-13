d66ec2269c1dfa52c70295321ca098279501da1a
Roberto Herrera <betohr48@hotmail.com>
2022-12-13 14:13:17 -0600

    base de datos

--- src/config/config.php
+++ src/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
     define('DB_HOST','localhost:3306');
-    define('DB_USER','root');
-    define('DB_PASS','');
+    define('DB_USER','cineadmin');
+    define('DB_PASS','admin');
     define('DB_NAME','VIDEOTECA');
     define('DB_CHARSET','utf8mb4');
 
